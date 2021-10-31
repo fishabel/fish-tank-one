a6c0cbcb7b2e560748a5041ccf6c771afa6fdc70
fishabel <86329855+fishabel@users.noreply.github.com>
2021-10-31 19:18:47 -0400

    final product

--- HW3.py
+++ HW3.py
@@ -1,51 +1,56 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[8]:
-
-
+import imageio as iio
 import numpy as np
-ROWS = 220  
-COLS =  255
-fin = open("/Users/abelng/Desktop/2021fall/CS688 Web Analy & Mining/Homework 3/Bug Final.png")
-print(fin)
 
+# read bug image as numpy array
+bugimg = np.asarray(iio.imread("C:/Users/abel/Desktop/Github/fish-tank-one/Bug Final_0.1x.png"))
+bugimg.astype(int)
+print(bugimg.shape)
 
-# In[9]:
+# read prepared chicken with bugs image as numpy array
+testimg = np.asarray(iio.imread("C:/Users/abel/Desktop/Github/fish-tank-one/chicken_w_bugs_0.1x.png"))
+testimg.astype(int)
+print(testimg.shape)
 
+#convert both images into 2D array [:,:,0]
+bugimg = bugimg[:,:,0]
+print(bugimg.shape)
+testimg = testimg[:,:,0]
+print(testimg.shape)
 
-# Loading the input image
-print("... Load input image")
-img = np.fromfile(fin, dtype = np.uint8, count = ROWS * COLS)
-print("Dimension of the old image array: ", img.ndim)
-print("Size of the old image array: ", img.size)
+# convert all the values to either 1 or 0
+bugimg[bugimg < 128] = 1
+bugimg[bugimg >= 128] = 0
+testimg[testimg < 128] = 1
+testimg[testimg >= 128] = 0
 
+# this step is just to make sure my converted pictures make sense.
+import matplotlib.pyplot as plt
+plt.imshow(bugimg, interpolation='none')
+plt.show()
+plt.imshow(testimg, interpolation='none')
+plt.show()
 
-# In[10]:
+# Compare bugimg array with sliced windows of testimg array to find the bug image
+test_window_emp = np.zeros((36,45))
+test_window_emp = test_window_emp.astype(int)
+i_list = []
+m_list = []
+for i in range(testimg.shape[0]-36+1):
+    for m in range(testimg.shape[1]-45+1):
+        test_window_cur = testimg[i:i+36, m:m+45]
+        #print(test_window_cur)
+        test_progress = np.add(test_window_emp,test_window_cur)
+        #print(test_progress)
+        if np.array_equal(test_progress, bugimg):
+            print("Found a bug at: ", i, ",", m)
+            i_list.append(i)
+            m_list.append(m)
 
+# Mark out the bugs in the original test image
+print(i_list)
+result_img = np.asarray(iio.imread("C:/Users/abel/Desktop/Github/fish-tank-one/chicken_w_bugs_0.1x.png"))
+for n in range(len(i_list)):
+        result_img[i_list[n]:i_list[n]+36,m_list[n]:m_list[n]+45, 1:3] = 0
 
-# Conversion from 1D to 2D array
-img.shape = (img.size // COLS, COLS)
-print("New dimension of the array:", img.ndim)
-print("----------------------------------------------------")
-print(" The 2D array of the original image is: \n", img)
-print("----------------------------------------------------")
-print("The shape of the original image array is: ", img.shape)
-
-
-# In[12]:
-
-
-# Save the output image
-print("... Save the output image")
-img.astype('int8').tofile('NewImage.raw')
-print("... File successfully saved")
-# Closing the file
-fin.close()
-
-
-# In[ ]:
-
-
-
-
+plt.imshow(result_img, interpolation='none')
+plt.show()
